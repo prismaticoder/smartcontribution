7bb0a1f1734b48670e85fdfdd3484d5b64706182
Prismathic <pjjsalam1999@gmail.com>
2019-06-07 17:50:04 +0100

    Started working on the index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require_once('partials/header.php'); 
+
+//Indicate that login is required to view this particular page
+
+
+?>
+
+<body>
+
+<header><h1 class="w3-center">SMART CONTRIBUTION APP</h1></header>
+
+</body>
+</html>
+
--- login.php
+++ login.php
@@ -1,4 +1,14 @@
-<?php require_once('partials/header.php'); ?>
+<?php require_once('partials/header.php'); 
+
+if (isset($_POST['submit'])) {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+    }
+}
+
+
+?>
 
 <body>
     <div class="container">
@@ -10,13 +20,13 @@
                         <h3><strong>SMART CONTRIBUTION ADMIN LOGIN</strong></h3>
                         <br>
                         <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" placeholder="Username">
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
                         <br>
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
                         <br>
                         <br>
-                        <button type="submit" class="btn btn-primary submit-btn">Login</button>
+                        <button type="submit" class="btn btn-primary submit-btn" name="submit">Login</button>
                     </div>
                 </form>
             </div>
