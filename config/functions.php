1e2d5b6ec22b3cbc1aa309d5bd17e8c61ad23db4
Prismathic <pjjsalam1999@gmail.com>
2019-06-07 23:16:57 +0100

    Queries for fetching username and password upon login

--- config/functions.php
+++ config/functions.php
@@ -6,6 +6,26 @@ function login($user, $pass) {
 
     //Convert password to md5 hash string
     $pass = md5($pass);
+
+    $query = "SELECT * FROM `users` WHERE username = $user AND password = $pass";
+    $result = mysqli_query($query) or die(mysqli_error());
+
+    $role_id = mysqli_fetch_assoc($result)['role_id'];
+    $role_query = "SELECT `role` FROM `roles` WHERE role_id = $role_id";
+    $role = mysqli_query($role_query) or die(mysqli_error());
+    
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION['authorized'] = true;
+        $_SESSION['role'] = $role ;
+
+        header('Location: '. DIR. 'index.php');
+        exit();
+    }
+    else {
+        echo ("<script>alert('Incorrect username or password entered!')</script>");
+    }
+
+    //I'm not through with this function yet...
 }
 //Function to check if a user is logged in
 function is_logged_in() {
@@ -15,20 +35,18 @@ function is_logged_in() {
     else {
         return false;
     }
-}
+};
 //Function to check if a user is logged in, if not, redirect to the login page
 function login_required() {
     if(is_logged_in()) {
         return true;
     }
-    else if (!is_logged_in && $_SERVER['REQUEST_URI'] !== '/smartcontribution/login.php') {
+    else {
         header('Location: '.DIR.'login.php');
         exit();
     }
-    else {
-        return true;
-    }
-}
+};
+
 //Function to Log a user out
 function logout() {
     if (isset($_GET['logout'])) {
