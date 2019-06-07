8c8db170b93dd616154696a0ac95abc7175918df
Prismathic <pjjsalam1999@gmail.com>
2019-06-07 10:33:28 +0100

    Began Work on Login File

--- login.php
+++ login.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+<?php require_once('partials/header.php') ?>
+
 <body>
-    
+    <form method="POST" action="index.php">
+    <div class="form-group">
 </body>
-</html>
