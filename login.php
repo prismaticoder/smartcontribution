567966a850288dc9dc06026f4495568196703520
Prismathic <pjjsalam1999@gmail.com>
2019-06-10 11:48:18 +0100

    Standalone header for the login page

--- login.php
+++ login.php
@@ -1,22 +1,40 @@
-<?php require_once('partials/header.php'); 
+<?php require_once('config/config.php'); 
 
 if (isset($_POST['submit'])) {
     if (isset($_POST['username']) && isset($_POST['password'])) {
         $user = $_POST['username'];
         $pass = $_POST['password'];
+
+        login($user,$pass);
     }
 }
 
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/myStyles.css">
+    <title>Smart Contribution | Admin Login </title>
+</head>
+
+<body>
+
+<header class="w3-container"><h2><a href="index.php"><img src="lib/smart-icon.png" width="50" height="50"/><i> Smart Contribution App</i></a></h2></header>
+<hr>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-3"></div>
             <div class="col-md-6">
-                <form method="POST" action="index.php">
-                    <div class="form-group login-div w3-border-blue-grey">
+                <form method="POST">
+                    <div class="form-group well login-div w3-border-blue-grey">
                         <h3><strong>SMART CONTRIBUTION ADMIN LOGIN</strong></h3>
                         <br>
                         <label for="username">Username</label>
