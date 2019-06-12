3f703064ba7f35115abe98e6ce5f045b67fc04dd
Prismathic <pjjsalam1999@gmail.com>
2019-06-13 00:02:04 +0100

    Added required js files to the footer

--- partials/footer.php
+++ partials/footer.php
@@ -5,3 +5,9 @@
 //Include your name and number as the designer/developer
 
 ?>
+
+<script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>      
+  
+    </body>
+    </html>
