643f29dbf835db48689c959d1862e2d24142e9c7
Prismathic <pjjsalam1999@gmail.com>
2019-06-12 22:03:03 +0100

    Finetuned the BE

--- viewAllCustomers.php
+++ viewAllCustomers.php
@@ -10,7 +10,15 @@ $customer_result = exec_query(
     ON main_customers.zone_id = zone.zone_id"
     );
 
+$zone_result = exec_query("SELECT zone FROM `zone` WHERE 1");
+$zones = [];
+while ($zone_rows = mysqli_fetch_assoc($zone_result)) {
+    $zones[] = $zone_rows['zone'];
+};
+
 $count = 1;
+
+// Only view customer should not be a modal, the rest (add new customer and edit existing customer should be in form of a modal)
 
 ?>
 
@@ -38,63 +46,68 @@ $count = 1;
             <td>" . $customer_rows['reg_date'] . "</td>
             <td>" . $customer_rows['zone'] . "</td>
             <td style='text-align:center'> 
-            <a href='/customer.php?custNo=". $customer_rows['customer_id'] ."'<i class='fa fa-external-link click-btn view'></i></a> 
-            <a data-toggle=\"modal\" href=\"#editJobModal". $customer_rows['customer_id'] ."\"><i class='fa fa-pencil click-btn edit'></i></a> 
+            <a title='View Customer Details' href='/customer.php?custNo=". $customer_rows['customer_id'] ."'<i class='fa fa-external-link click-btn view'></i></a> 
+            <a title='Edit Customer Details' data-toggle=\"modal\" href=\"#editJobModal". $customer_rows['customer_id'] ."\"><i class='fa fa-pencil click-btn edit'></i></a> 
             <i class='fa fa-close click-btn delete'></i> </td>
             </tr>
-            <div class='modal fade' id='editJobModal". $customer_rows['customer_id'] ."' tabindex='-1' role='dialog' aria-labelledby='editJobModalLabel' aria-hidden='true'>
-            <div class='modal-dialog ' role='document'>
-              <div class='modal-content'>
-                <div class='modal-header bg-light'>
-                  <!-- Create Job Modal-->
-                  <h5 class='modal-title' id='editJobModalLabel'>Customer #". $customer_rows['customer_id'] ."</h5>
-                  <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
-                    <span aria-hidden='true'>&times;</span>
-                  </button>
-                </div>
-                <div class='modal-body'>
-                  <div class='nrm-wrapper'>
-                    <div class='form-group'>
-                        <label for='card_no'>Card No</label>
-                      <input type='text' name='card_no' value='". $customer_rows['customer_id'] . "' class='form-control br-0'>
-                    </div>
-                    <div class='form-group'>
-                    <label for='cust_name'>Customer Name</label>
-                      <input type='text' name='cust_name' value='". $customer_rows['customer_name'] . "' class='form-control br-0'>
-                    </div>
-                    <div class='form-group'>
-                    <label for='cust_no'>Phone Number</label>
-                      <input type='number' name='cust_no' value='". $customer_rows['customer_phone_num'] . "' class='form-control br-0'>
-                    </div>
-                    <div class='row'>
-                      <div class='col-lg-6'>
-                        <div class='form-group'>
-                    <label for='zone'>Zone</label>
-                    <select name='zone' id='' class='form-control br-0'>
-                        <option selected>". $customer_rows['zone'] . "</option>
-                        <option value=''>Junior</option>
-                        <option value=''>Intermidiate</option>
-                        <option value=''>Expert</option>
-                      </select>
+
+            <div class='modal fade' id='editJobModal". $customer_rows['customer_id'] ."' tabindex='-1' role='dialog' aria-labelledby='editJobModalLabel' data-backdrop='false' aria-hidden='true'>
+                <div class='modal-dialog ' role='document'>
+                    <div class='modal-content'>
+                        <div class='modal-header bg-light'>
+                        <!-- Create Job Modal-->
+                            <h5 class='modal-title' id='editJobModalLabel'>Customer #". $customer_rows['customer_id'] ."</h5>
+                            <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                            <span aria-hidden='true'>&times;</span>
+                            </button>
                         </div>
-                      </div>
-                      <div class='col-lg-6'>
-                        <div class='form-group'>
-                        <label for='reg_date'>Registration Date</label>
-                          <input name='reg_date' value='" . $customer_rows['reg_date'] . "' class='form-control br-0'>
+                    <div class='modal-body'>
+                        <div class='nrm-wrapper'>
+                            <div class='form-group'>
+                                <label for='card_no'>Card No</label>
+                                <input type='text' name='card_no' value='". $customer_rows['customer_id'] . "' class='form-control br-0'>
+                            </div>
+                            <div class='form-group'>
+                                <label for='cust_name'>Customer Name</label>
+                                <input type='text' name='cust_name' value='". $customer_rows['customer_name'] . "' class='form-control br-0'>
+                            </div>
+                            <div class='form-group'>
+                                <label for='cust_no'>Phone Number</label>
+                                <input type='number' name='cust_no' value='". $customer_rows['customer_phone_num'] . "' class='form-control br-0'>
+                            </div>
+                            <div class='row'>
+                                <div class='col-lg-6'>
+                                    <div class='form-group'>
+                                        <label for='zone'>Zone</label>
+                                        <select name='zone' id='' class='form-control br-0'>";
+                                            foreach ($zones as $zone) {
+                                                if ($zone == $customer_rows['zone']) {
+                                                    echo "<option selected>".$zone."</option>";
+                                                }
+                                                else {
+                                                    echo "<option>".$zone."</option>";
+                                                }
+                                            };
+                                       echo "</select>
+                                    </div>
+                                </div>
+                                <div class='col-lg-6'>
+                                    <div class='form-group'>
+                                        <label for='reg_date'>Registration Date</label>
+                                        <input name='reg_date' value='" . $customer_rows['reg_date'] . "' class='form-control br-0'>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='col-12'>
+                                    <input type='submit' name='update' class='submit btn btn-info b-7 br-0 btn-block' value='Save Changes'>
+                                </div>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                    <div class='row'>
-                      <div class='col-12'>
-                        <input type='submit' class='submit btn btn-info b-7 br-0 btn-block' value='Save Changes'>
-                      </div>
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
-          </div>";
+            </div>";
             $count++;
         }
             ?>
