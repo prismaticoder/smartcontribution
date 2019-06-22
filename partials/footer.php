4df5901be65f2dd3deb02ce9fa65023fd5d2898d
Prismathic <pjjsalam1999@gmail.com>
2019-06-22 07:00:17 +0100

    Footer to include app name and designers name

--- partials/footer.php
+++ partials/footer.php
@@ -1,12 +1,14 @@
-<?php
 
-//Echo the current year
-//Add angular js files
-//Include your name and number as the designer/developer
-
-?>
-
-      
-  
-    </body>
-    </html>
+<footer class="container-fluid powder-blue w3-text-light-gray w3-padding-16">
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-5">
+            &copy;<?php echo date('Y');?> The Payday App <br>
+            Designed and Developed for Production by <a class="" href="https://github.com/prismathic">Prism Web Designs</a> (07035048364) 
+        </div>
+        <div class="col-lg-3"></div>
+    </div>
+</footer>
+        
+</body>
+</html>
