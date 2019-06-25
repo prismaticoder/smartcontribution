634172f3292a4dcb75ec36414d8fa49a54d248eb
Prismathic <pjjsalam1999@gmail.com>
2019-06-26 00:24:52 +0100

    added the query to be executed

--- admin_section.php
+++ admin_section.php
@@ -2,7 +2,10 @@
 
 require_once('partials/header.php');
 
+$result = exec_query("SELECT users.role_id,users.username,users.password,role.role FROM users INNER JOIN roles ON users.role_id = roles.role_id")
+
 ?>
+
 
 
 <?php
