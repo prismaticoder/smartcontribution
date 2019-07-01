eaed6f3f3e990f6a871609a89bba38f3f1b95029
Prismathic <pjjsalam1999@gmail.com>
2019-07-01 20:20:13 +0100

    Little FE fixes on the footer

--- partials/footer.php
+++ partials/footer.php
@@ -2,11 +2,11 @@
 <footer class="container-fluid powder-blue w3-text-light-gray w3-padding-16">
     <div class="row">
         <div class="col-lg-4"></div>
-        <div class="col-lg-5">
+        <div class="col-lg-6">
             &copy;<?php echo date('Y');?> The Payday App <br>
             Designed and Developed for Production by <a class="" href="https://github.com/prismathic">Prism Web Designs</a> (07035048364) 
         </div>
-        <div class="col-lg-3"></div>
+        <div class="col-lg-2"></div>
     </div>
 </footer>
         
